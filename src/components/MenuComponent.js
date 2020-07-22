cb93bfa9361dbff87599f57932c07aca934ae759
bhavesh <bkpassi2017@gmail.com>
2020-07-23 01:50:58 +0530

    Fetch From Server

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
-    CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem, CardBody} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
             <Card>
                 <Link to={`/menu/${dish.id}`} >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg width="100%" src={ baseUrl + dish.image} alt={dish.name} />
                     <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
+                        <CardTitle className="text-dark">{dish.name}</CardTitle>
+                        <CardBody className="text-dark">{'Click me for further details..'}</CardBody>
                     </CardImgOverlay>
                 </Link>
             </Card>  
