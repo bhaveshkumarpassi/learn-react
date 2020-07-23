bf221d71db0ec3f5f4d6a340c331869761f136d7
bhavesh <bkpassi2017@gmail.com>
2020-07-23 18:07:56 +0530

    React Animations

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem, Label, Col,
          Button,Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Form, Control, Errors, actions } from 'react-redux-form';
+import { Form, Control, Errors } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
